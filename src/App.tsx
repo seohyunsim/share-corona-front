d00375ad34e7b2df7f70523123c2e73d2d76edbb
seohyunsim <ssh4563@naver.com>
2022-03-07 14:00:34 +0900

    Add(main): Link로 라우팅시 스크롤 top으로 가도록 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Main } from "./components/Common/Main";
+import { ScrollToTop } from "./components/Common/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop />
         <Main />
       </Router>
     </div>
